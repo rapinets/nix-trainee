8456f57e1d00e7451e602e8f74497960e3458dea
Vasiliy Rapinec <55457500+rapinets@users.noreply.github.com>
2021-08-07 16:40:26 +0300

    Create view pages

---
+++ menu.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Posts</a></li>
+        <li><a href="">Link 3</a></li>
+        <li><a href="">Link 4</a></li>
+        <li><a href="">About us</a></li>
+    </ul>
+</nav>
